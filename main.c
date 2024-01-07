306d38d2cf113d6a1a50910eb7ca3e002c3ae307
uyanda-maseti <masetibabalo2@gmail.com>
2024-01-07 22:15:07 +0200

    Create main.c file and code counter.

--- main.c
+++ main.c
@@ -3,16 +3,8 @@
 int main(void) 
 {
     
-    int age = 18;
-
-    if ( age < 18) 
-    {
-        printf("You are young.\n");
-    }
-    else
-    {
-        printf("You are an adult.\n");
-    }
+    int counter = 0;
+    printf("counter = %i", counter);
 
     return 0;
 }
